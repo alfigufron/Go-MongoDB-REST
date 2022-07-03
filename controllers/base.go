4263be756ed74cb31155be2382c6af17ce8c8caa
Athhar Kautsar <athharkautsar14@gmail.com>
2022-07-03 16:38:30 +0700

    fix(response): refactor response helper

--- controllers/base.go
+++ controllers/base.go
@@ -1,14 +1,10 @@
 package controllers
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/hexa/go-boilerplate-restapi/helpers"
 )
 
 func Ping(c *gin.Context) {
-	payload := helpers.ResponseBody("success", "Ping Successfully", nil, 200)
-
-	c.JSON(http.StatusOK, payload)
+	helpers.Response(c, "Ping Successfully!!", nil, 200)
 }
